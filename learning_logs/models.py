49520f9423a9f1164f34d6f38c887f9a90bbe8d0
YaRo <roman.yatsenko@hneu.net>
2025-06-07 16:15:10 +0300

    Add Topic model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    """Тема, яку вивчає користувач"""
+
+    text = models.CharField(max_length=255, verbose_name="Назва теми")
+    date_added = models.DateTimeField(auto_now_add=True, verbose_name="Створено")
+
+    def __str__(self):
+        return self.text
