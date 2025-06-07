80ceb2939e59ea6e4eed15b80f4b91458d738fab
YaRo <roman.yatsenko@hneu.net>
2025-06-07 17:29:27 +0300

    Add Entry model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -8,5 +8,25 @@ class Topic(models.Model):
     text = models.CharField(max_length=255, verbose_name="Назва теми")
     date_added = models.DateTimeField(auto_now_add=True, verbose_name="Створено")
 
+    class Meta:
+        verbose_name = "Тема"
+        verbose_name_plural = "Теми"
+
     def __str__(self):
         return self.text
+
+
+class Entry(models.Model):
+    """Дописи за темами"""
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, verbose_name="Тема")
+    text = models.TextField(verbose_name="Текст")
+    date_added = models.DateTimeField(auto_now_add=True, verbose_name="Створено")
+
+    class Meta:
+        verbose_name = "Допис"
+        verbose_name_plural = "Дописи"
+
+    def __str__(self):
+        return f"{self.text[:50]}..."
+    
