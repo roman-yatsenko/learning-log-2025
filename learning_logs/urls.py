9a389ab5c001ab0962281c55c4ef4bd2e0cbd1bf
YaRo <roman.yatsenko@hneu.net>
2025-06-14 16:47:31 +0300

    Add Topic page

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Сторінка зі списком усіх тем
     path('topics/', views.topics, name='topics'),
+    # Сторінка з інформацією за окремою темою
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -13,3 +13,13 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Виводе одну тему і всі її дописи"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {
+        'topic': topic,
+        'entries': entries,
+    }
+    return render(request, 'learning_logs/topic.html', context)
