19ac61c9c626fbf70b4158fd502d122fcdd79e07
YaRo <roman.yatsenko@hneu.net>
2025-06-21 17:31:03 +0300

    Add link between Topic and User

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -7,6 +8,7 @@ class Topic(models.Model):
 
     text = models.CharField(max_length=255, verbose_name="Назва теми")
     date_added = models.DateTimeField(auto_now_add=True, verbose_name="Створено")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Власник")
 
     class Meta:
         verbose_name = "Тема"
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
+from django.shortcuts import redirect, render
 
 from .forms import EntryForm, TopicForm
 from .models import Entry, Topic
@@ -13,13 +14,16 @@ def index(request):
 @login_required
 def topics(request):
     """Виводе список тем"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """Виводе одну тему і всі її дописи"""
     topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {
         'topic': topic,
@@ -27,6 +31,7 @@ def topic(request, topic_id):
     }
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """Додає нову тему"""
     if request.method != 'POST':
@@ -34,12 +39,15 @@ def new_topic(request):
     else:
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
     
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """Додає новий допис до визначеної теми"""
     topic = Topic.objects.get(id=topic_id)
@@ -60,10 +68,13 @@ def new_entry(request, topic_id):
     }
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Редагує існуючий допис"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         form = EntryForm(instance=entry)
