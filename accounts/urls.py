3203846aa951e06384a527363279603d3cf7c24a
YaRo <roman.yatsenko@hneu.net>
2025-06-21 16:41:44 +0300

    Add registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import include, path
 
+from . import views
+
 app_name = 'accounts'
 urlpatterns = [
     # Додати URL авторизації за замовчуванням
     path('', include('django.contrib.auth.urls')),
+    # Сторінка реєстрації
+    path('register/', views.register, name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
 
 # Create your views here.
+
+def register(request):
+    """Реєструє нового користувача"""
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return redirect('learning_logs:index')
+    
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
